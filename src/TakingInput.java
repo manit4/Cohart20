79e3c3d4a767e77684858aac61801866a1c7e7bc
Manit <manit.manu4@gmail.com>
2022-10-04 21:49:36 +0530

    firstAssignment

--- src/TakingInput.java
+++ src/TakingInput.java
@@ -6,28 +6,46 @@ public class TakingInput {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		boolean flag = true;
+		System.out.println("Enter any name which you wish to print!!");
 		
-		while(flag) {
+		String name = sc.nextLine();
+		
+		System.out.println("Enter any number!!");
+		
+		int number = sc.nextInt();
+		
+		for(int i = 0; i < number; i++) {
 			
-			System.out.println("Enter the name");
-			
-			String name = sc.next();
-			
-			if(name.equals("Ama")) {
-				System.out.println("You have successfully logged-in..");
-				flag = false;
-			}
-			else {
-				System.out.println("Sorry, credentials wrong..");
-			}	
-		}
-			
-			
-		
-		
-		
-		
-		
+			System.out.println(name);
+		}	
 	}
 }
+
+
+
+//import java.util.Scanner;
+//
+//public class TakingInput {
+//	
+//	public static void main(String[] args) {
+//		
+//		Scanner sc = new Scanner(System.in);
+//		
+//		boolean flag = true;
+//		
+//		while(flag) {
+//			
+//			System.out.println("Enter the name");
+//			
+//			String name = sc.next();
+//			
+//			if(name.equals("Ama")) {
+//				System.out.println("You have successfully logged-in..");
+//				flag = false;
+//			}
+//			else {
+//				System.out.println("Sorry, credentials wrong..");
+//			}	
+//		}	
+//	}
+//}
---
+++ src/User.java
@@ -0,0 +1,9 @@
+
+public class User {
+	
+	String userId;
+	String firstName;
+	String lastName;
+	
+
+}
